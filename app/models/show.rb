416bcc726d8f33252964349ec2d175c4a417cd31
angelinesimiyu <angie.lukelas@gmail.com>
2023-02-28 18:35:01 +0300

    Completed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+      actors.full_name.each do |actor|
+          "#{actor.&:full_name} - #{actor.show}"
+      end
+    end
+end
+
 end
